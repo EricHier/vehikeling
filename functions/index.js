01d04e9697ced7033438f9121835a6acef1f56e5
EricHier <19eric03@gmail.com>
2020-01-12 20:44:48 +0100

    Backup

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
-/*const functions = */require('firebase-functions');
+const functions = require('firebase-functions');
+const firestore = require('@google-cloud/firestore');
+const client = new firestore.v1.FirestoreAdminClient();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Replace BUCKET_NAME
+const bucket = 'gs://backup-firestore-vehikeling';
+
+exports.scheduledFirestoreExport = functions.pubsub
+  .schedule('0 2 */1 * *')
+  .onRun((context) => {
+    const databaseName =
+      client.databasePath(process.env.GCP_PROJECT, '(default)');
+
+    return client.exportDocuments({
+      name: databaseName,
+      outputUriPrefix: bucket,
+      // Leave collectionIds empty to export all collections
+      // or set to a list of collection IDs to export,
+      // collectionIds: ['users', 'posts']
+      collectionIds: []
+    })
+      .then(responses => {
+        const response = responses[0];
+        console.log(`Operation Name: ${response['name']}`);
+        return response;
+      })
+      .catch(err => {
+        console.error(err);
+        throw new Error('Export operation failed');
+      });
+  });
