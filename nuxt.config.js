ea30fc1fd5fee130c29d519a8b2376f2be4d080a
EricHier <19eric03@gmail.com>
2020-01-12 20:44:48 +0100

    Backup

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     title: 'vehikeling | Verwaltung für die Services deiner Fahrzeuge',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: 'Die Vehikel-Servecing App für deine Sportwagen, Oldtimer oder Motorräder - ganz unkompliziert die Services wie Ölwechsel und HU verwalten. Probiere es aus!' },
     ],
     link: [
