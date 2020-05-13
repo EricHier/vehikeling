88db7a656da98c49bfb187711dbe79a73e7f286a
erichier <developer.erichier@gmail.com>
2020-05-13 22:57:18 +0200

    added icons and firebase api key

--- components/gui-components/Footer.vue
+++ components/gui-components/Footer.vue
@@ -1,12 +1,15 @@
 <template>
   <footer class="pt-12 pb-24 px-6 bg-footer print-disable">
-    <div class="container mx-auto text-center">
+    <div class="container text-center">
       <p class="mb-4">
         <strong>vehikeling</strong> by EricHier.
       </p>
       <p>
-        <nuxt-link class="text-red-500 font-semibold" to="/datenschutz">Datenschutzhinweise</nuxt-link> |
-        <nuxt-link class="text-red-500 font-semibold" to="/impressum">Impressum</nuxt-link>
+        Icons von <a class="text-link" href="https://icons8.com">Icons8</a>.
+      </p>
+      <p>
+        <nuxt-link class="text-link" to="/datenschutz">Datenschutzhinweise</nuxt-link> |
+        <nuxt-link class="text-link" to="/impressum">Impressum</nuxt-link>
       </p>
     </div>
   </footer>
---
+++ services/firebase-api.js
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyAuICgaGihRlqLNL97e4dOwAS060ZbHenw",
+  authDomain: "carmanager-erichier.firebaseapp.com",
+  databaseURL: "https://carmanager-erichier.firebaseio.com",
+  projectId: "carmanager-erichier",
+  storageBucket: "carmanager-erichier.appspot.com",
+  messagingSenderId: "999888665498",
+  appId: "1:999888665498:web:e1b922c1cfa9d7ca191823",
+  measurementId: "G-Z2H7N09YT9"
+};
